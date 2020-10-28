262537ceafe5e3dc3ddda891a13130a65aac6803
vimal Raveendran <vimalraveendra@gmail.com>
2020-10-28 09:55:57 +0100

    Created Containers Folder and Moved App components & its styles to it, also created Components folder& added UserData Component

---
+++ src/containers/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+const App =() =>{
+  return (
+    <div className="container">
+    <h1>React CRUD App with Hooks</h1>
+      <div className="row">
+        <div className="five columns">
+          <h2>Add user</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="five columns">
+          <h2>Add user</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
