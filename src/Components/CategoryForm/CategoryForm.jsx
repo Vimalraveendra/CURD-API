9440336be33d3d690eb0a51ffc9fa0bf84733d45
vimal Raveendran <vimalraveendra@gmail.com>
2020-10-28 14:54:52 +0100

    Added CategoryForm component & its stylesheet,also modified the categoryTable component & App component to add ,delete , update & List categories

---
+++ src/Components/CategoryForm/CategoryForm.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const CategoryForm = ({addUserCategory,handleChange,user,editData,handleCancel}) => {
+   
+    
+    // const initialUser = {id: null, name: '', description: ''};
+
+    // const [user, setUser] = useState(initialUser);
+
+    // const handleChange=(event)=>{
+    //     const {name,value} = event.target;
+    //     setUser({...user,[name]:value})
+    // }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+     if(user.name&& user.description){
+         addUserCategory(user)  
+         
+     }
+    }
+
+    return (
+        <form>
+            <label>Name</label>
+            <input className="u-full-width" type="text" name="name" value={user.name} onChange={handleChange}/>
+            <label>Description</label>
+            <input className="u-full-width" type="text" name="description" value={user.description} onChange={handleChange}/>
+            {editData?(
+            <div>
+            <button className="button-primary" type="submit"  onClick={handleSubmit}>Edit User</button>
+            <button className="edit" type="submit" onClick={handleCancel} >cancel</button>
+            </div>
+            ):(
+                <button className="button-primary" type="submit" onClick={handleSubmit}>Add User</button>
+            )
+            }
+        </form>
+    )
+}
+
+export default CategoryForm;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,24 +2,85 @@ import React from 'react';
 import './App.css';
 import UserData from '../Components/UserData/UserData';
 import CategoryTable from '../Components/CategoryTable/CategoryTable'
+import CategoryForm from '../Components/CategoryForm/CategoryForm'
+
 
 
 class App extends React.Component{
   state={
-    userData:UserData
+    userData:UserData,
+    editData:false,
+    user:{
+      id:null,
+      name:'',
+      description:''
+    }
   }
  
-  render(){
+  // adding new category list
+  addUserCategory=(user)=>{
+    if(!user.id){
+    user.id=this.state.userData.length+1
+    }
+    let newData=[...this.state.userData,user]
+    this.setState({userData:newData})
+  }
+
+  // Deleting category list based on id
+  deleteUser=id=>{
+    let newList =this.state.userData.filter(user=>user.id!==id);
+    this.setState({userData:newList}) 
+  }
+
+  handleChange=(event)=>{
+    const {name,value} = event.target;
+    const newUser= {...this.state.user,[name]:value};
+     this.setState({user:newUser})
+
+}
+
+
+handleCancel=(e)=>{
+  e.preventDefault();
+  this.state({editData:!this.state.editData})
+}
+
+
+  editUser=id=>{
+    let newList =this.state.userData.filter(user=>user.id!==id)
+    const selectedItem = this.state.userData.find(item=>item.id === id)
+    this.setState({
+      userData:newList,
+      user:selectedItem,
+      editData:true
+    })
+    
+  }
+
+
+  render(){ 
     return (
       <div className="container">
-      <h1>React CRUD App</h1>
+      <h1> CRUD APP</h1>
         <div className="row">
           <div className="five columns">
-            <h2>Add Category</h2>
+          {this.state.editData?( 
+              <div>
+              <h2>Edit Category</h2>
+                <CategoryForm addUserCategory={this.addUserCategory} handleChange={this.handleChange} user={this.state.user} editData={this.state.editData}/>
+                </div>
+                ):
+                (
+                  <div>
+                  <h2>Add Category</h2>
+                  <CategoryForm addUserCategory={this.addUserCategory} handleChange={this.handleChange} user={this.state.user} handleCancel />
+                  </div>
+                  )
+          }
           </div>
           <div className="seven columns">
             <h2>View Category</h2>
-            <CategoryTable userData={this.state.userData}/>
+            <CategoryTable userData={this.state.userData} deleteUser={this.deleteUser} editUser={this.editUser} />
           </div>
         </div>
       </div>
