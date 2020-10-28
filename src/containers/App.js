f64ff9bc96b3224ca7330f6e89c820524ff2f50e
vimal Raveendran <vimalraveendra@gmail.com>
2020-10-28 11:04:47 +0100

    Created CategoryTable component to render the categroyList,create styles for the categoryTable component & also updated the App component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,32 @@
-
+import React from 'react';
 import './App.css';
+import UserData from '../Components/UserData/UserData';
+import CategoryTable from '../Components/CategoryTable/CategoryTable'
 
-const App =() =>{
-  return (
-    <div className="container">
-    <h1>React CRUD App with Hooks</h1>
-      <div className="row">
-        <div className="five columns">
-          <h2>Add user</h2>
+
+class App extends React.Component{
+  state={
+    userData:UserData
+  }
+ 
+  render(){
+    return (
+      <div className="container">
+      <h1>React CRUD App</h1>
+        <div className="row">
+          <div className="five columns">
+            <h2>Add Category</h2>
+          </div>
+          <div className="seven columns">
+            <h2>View Category</h2>
+            <CategoryTable userData={this.state.userData}/>
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="five columns">
-          <h2>Add user</h2>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+  
+
 
 export default App;
